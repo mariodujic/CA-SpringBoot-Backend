b56fdc4d72a9441166c85f72ad9835a741746f41
Mario Dujić <mario.dujic@gmail.com>
2020-08-26 17:26:36 +0200

    Local data updated every 60 min

--- src/main/java/com/groundzero/camw/cachier/CacheController.kt
+++ src/main/java/com/groundzero/camw/cachier/CacheController.kt
@@ -4,6 +4,10 @@ import com.groundzero.camw.prayers.constants.PRAYER_EN_COLLECTION
 import com.groundzero.camw.quizzes.constants.QUIZ_EN_COLLECTION
 import com.groundzero.camw.thoughts.constants.THOUGHT_EN_COLLECTION
 import com.groundzero.camw.utils.getJsonLog
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
@@ -15,10 +19,21 @@ class CacheController : CommandLineRunner {
     private lateinit var repository: CacheRepository
 
     override fun run(vararg args: String?) {
-        // updateQuizzes()
+        CoroutineScope(IO).launch {
+            while (true) {
+                delay(LOCAL_DATA_UPDATE_DELAY_MIL)
+                updatePrayers()
+                updateThoughts()
+                updateQuizzes()
+            }
+        }
     }
 
     private fun updateQuizzes() = repository.readNetworkQuizzes()?.let { repository.writeJsonQuizzes(it).getJsonLog(QUIZ_EN_COLLECTION) }
     private fun updatePrayers() = repository.readNetworkPrayers()?.let { repository.writeJsonPrayers(it).getJsonLog(PRAYER_EN_COLLECTION) }
     private fun updateThoughts() = repository.readNetworkThoughts()?.let { repository.writeJsonThoughts(it).getJsonLog(THOUGHT_EN_COLLECTION) }
+
+    companion object {
+        private const val LOCAL_DATA_UPDATE_DELAY_MIL = 3_600_000L // 60 min
+    }
 }
