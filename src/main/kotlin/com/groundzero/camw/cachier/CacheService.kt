38848014af915e0f467ac3e7ff0326c2c9bb691a
Mario Dujić <mario.dujic@gmail.com>
2020-08-30 07:30:53 +0200

    Refactor CacheService

--- src/main/kotlin/com/groundzero/camw/cachier/CacheService.kt
+++ src/main/kotlin/com/groundzero/camw/cachier/CacheService.kt
@@ -22,17 +22,23 @@ class CacheService(
 ) : Cache {
 
     override fun updateQuizzes() {
-        readNetwork.readDatabase<QuizCategory>(QUIZ_EN_COLLECTION)?.let { writeJson.writeJson(getJsonStoragePath(QUIZ_EN_COLLECTION), it).getJsonLog(QUIZ_EN_COLLECTION) }
-        readNetwork.readDatabase<QuizCategory>(QUIZ_HR_COLLECTION)?.let { writeJson.writeJson(getJsonStoragePath(QUIZ_HR_COLLECTION), it).getJsonLog(QUIZ_HR_COLLECTION) }
+        updateData<QuizCategory>(QUIZ_EN_COLLECTION)
+        updateData<QuizCategory>(QUIZ_HR_COLLECTION)
     }
 
     override fun updatePrayers() {
-        readNetwork.readDatabase<Prayer>(PRAYER_EN_COLLECTION)?.let { writeJson.writeJson(getJsonStoragePath(PRAYER_EN_COLLECTION), it).getJsonLog(PRAYER_EN_COLLECTION) }
-        readNetwork.readDatabase<Prayer>(PRAYER_HR_COLLECTION)?.let { writeJson.writeJson(getJsonStoragePath(PRAYER_HR_COLLECTION), it).getJsonLog(PRAYER_HR_COLLECTION) }
+        updateData<Prayer>(PRAYER_EN_COLLECTION)
+        updateData<Prayer>(PRAYER_HR_COLLECTION)
     }
 
     override fun updateThoughts() {
-        readNetwork.readDatabase<Thought>(THOUGHT_EN_COLLECTION)?.let { writeJson.writeJson(getJsonStoragePath(THOUGHT_EN_COLLECTION), it).getJsonLog(THOUGHT_EN_COLLECTION) }
-        readNetwork.readDatabase<Thought>(THOUGHT_HR_COLLECTION)?.let { writeJson.writeJson(getJsonStoragePath(THOUGHT_HR_COLLECTION), it).getJsonLog(THOUGHT_HR_COLLECTION) }
+        updateData<Thought>(THOUGHT_EN_COLLECTION)
+        updateData<Thought>(THOUGHT_HR_COLLECTION)
+    }
+
+    private inline fun <reified T> updateData(collectionKey: String) {
+        with(collectionKey) {
+            readNetwork.readDatabase<T>(this)?.let { writeJson.writeJson(getJsonStoragePath(this), it).getJsonLog(this) }
+        }
     }
 }
