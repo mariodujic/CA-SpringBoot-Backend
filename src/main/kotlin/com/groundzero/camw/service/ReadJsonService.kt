4e2ce894813c431b39bbe858f9f32444750c9ca4
Mario Dujić <mario.dujic@gmail.com>
2020-09-09 18:00:39 +0200

    Modify read json behaviour

--- src/main/kotlin/com/groundzero/camw/service/ReadJsonService.kt
+++ src/main/kotlin/com/groundzero/camw/service/ReadJsonService.kt
@@ -4,13 +4,23 @@ import com.groundzero.camw.utils.SerializationUtils
 import com.groundzero.camw.utils.getJsonStoragePath
 import org.springframework.stereotype.Component
 import java.io.File
+import java.io.FileNotFoundException
+
 
 @Component
 @Suppress("declaration_cant_be_inlined")
 class ReadJsonService(val serialization: SerializationUtils) {
     inline fun <reified T> read(path: String): List<T>? =
-            serialization.mapper.readValue(
-                    File(getJsonStoragePath(path)),
-                    serialization.mapper.typeFactory.constructCollectionType(MutableList::class.java, T::class.java)
-            )
+            try {
+                serialization.mapper.readValue(
+                        File(getJsonStoragePath(path)),
+                        serialization.mapper.typeFactory.constructCollectionType(MutableList::class.java, T::class.java)
+                )
+            } catch (e: FileNotFoundException) {
+                /**
+                 * If file is not created and new data is being added, file will be created.
+                 * @see WriteJsonService
+                 */
+                mutableListOf()
+            }
 }
