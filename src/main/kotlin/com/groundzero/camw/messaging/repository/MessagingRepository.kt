ab6fdb10d987547a75173e89ab1268ae87114809
Mario Dujić <mario.dujic@gmail.com>
2020-09-28 19:33:04 +0200

    Add authentication service

--- src/main/kotlin/com/groundzero/camw/messaging/repository/MessagingRepository.kt
+++ src/main/kotlin/com/groundzero/camw/messaging/repository/MessagingRepository.kt
@@ -2,20 +2,16 @@ package com.groundzero.camw.messaging.repository
 
 import com.google.api.core.ApiFuture
 import com.google.cloud.firestore.DocumentReference
-import com.google.firebase.auth.UserRecord
-import com.groundzero.camw.core.data.FirebaseAuthProvider
 import com.groundzero.camw.core.data.FirestoreProvider
 import com.groundzero.camw.messaging.data.Message
 import org.springframework.stereotype.Component
 
 @Component
 class MessagingRepository(
-        private val firebaseAuth: FirebaseAuthProvider,
         private val firestoreProvider: FirestoreProvider,
         private val messageToThoughtMapper: MessageToThoughtMapper
 ) {
 
-    fun authenticateUser(email: String): UserRecord = firebaseAuth.firebaseAuth.getUserByEmail(email)
     fun addMessage(collectionKey: String, message: Message): ApiFuture<DocumentReference> =
             firestoreProvider.firestore.collection(collectionKey).add(messageToThoughtMapper.map(message))
 }
