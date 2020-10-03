269c7cbe5bc7649e8524cca762ccece862def09c
Mario Dujić <mario.dujic@gmail.com>
2020-10-03 08:27:42 +0200

    Modify server date pattern

--- src/main/kotlin/com/groundzero/camw/utils/DateUtils.kt
+++ src/main/kotlin/com/groundzero/camw/utils/DateUtils.kt
@@ -5,5 +5,7 @@ import java.util.*
 
 object DateUtils {
 
-    fun currentDate(): String = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(Date())
+    fun currentDate(): String = SimpleDateFormat(DATE_FORMAT).format(Date())
+
+    private const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
 }
