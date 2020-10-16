d5b73c397f86ddf8f68b422c031269acf058b16d
Mario Dujić <mario.dujic@gmail.com>
2020-10-16 20:53:56 +0200

    Add Saint class properties

--- src/main/kotlin/com/groundzero/camw/features/saints/data/Saint.kt
+++ src/main/kotlin/com/groundzero/camw/features/saints/data/Saint.kt
@@ -5,6 +5,10 @@ import com.groundzero.camw.core.data.NetworkModel
 data class Saint(
         override val itemId: String? = null,
         val title: String? = null,
+        val subtitle: String? = null,
         val text: String? = null,
         val image: String? = null,
+        val created: String? = null,
+        val celebrated: String? = null,
+        val lived: String? = null
 ) : NetworkModel
