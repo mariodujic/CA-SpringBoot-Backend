9f6eca6962d9f1b945c5cb83e4c7bb89f1395807
Mario Dujić <mario.dujic@gmail.com>
2020-11-13 20:10:45 +0100

    Add AdConfig properties

--- src/main/kotlin/com/groundzero/camw/features/adconfig/data/AdConfig.kt
+++ src/main/kotlin/com/groundzero/camw/features/adconfig/data/AdConfig.kt
@@ -1,14 +1,31 @@
 package com.groundzero.camw.features.adconfig.data
 
 data class AdConfig(
+        // Main screen top banner
+        val showPrayerListTopBanner: Boolean = false,
+        val showThoughtListTopBanner: Boolean = false,
+        val showSaintListTopBanner: Boolean = false,
+        val showQuizzesListTopBanner: Boolean = false,
         // Main screen bottom banner
         val showPrayerListBottomBanner: Boolean = false,
         val showThoughtListBottomBanner: Boolean = false,
         val showSaintListBottomBanner: Boolean = false,
         val showQuizzesListBottomBanner: Boolean = false,
+        // Content reading top banner
+        val showSinglePrayerTopBanner: Boolean = false,
+        val showSingleThoughtTopBanner: Boolean = false,
         // Content reading bottom banner
         val showSinglePrayerBottomBanner: Boolean = false,
         val showSingleThoughtBottomBanner: Boolean = false,
         // Interstitial ads
-        val showExitApplicationInterstitial: Boolean = false
+        val showExitApplicationInterstitial: Boolean = false,
+        val showRandomPrayerInterstitial: Boolean = false,
+        val showPrayerListLayoutChangeInterstitial: Boolean = false,
+        val showThoughtListLayoutChangeInterstitial: Boolean = false,
+        val showAddPersonalPrayerInterstitial: Boolean = false,
+        val showBookmarksInterstitial: Boolean = false,
+        val showSearchPrayerListInterstitial: Boolean = false,
+        val showSearchThoughtListInterstitial: Boolean = false,
+        val showSearchSaintListInterstitial: Boolean = false,
+        val showQuizFinishInterstitial: Boolean = false
 )
