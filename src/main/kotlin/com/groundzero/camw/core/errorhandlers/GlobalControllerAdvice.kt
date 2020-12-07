f773dcb8e38f4db0efea48e40924c63494a3aa0c
Mario Dujić <mario.dujic@gmail.com>
2020-12-07 19:23:01 +0100

    Add bad request handler

--- src/main/kotlin/com/groundzero/camw/core/errorhandlers/GlobalControllerAdvice.kt
+++ src/main/kotlin/com/groundzero/camw/core/errorhandlers/GlobalControllerAdvice.kt
@@ -2,8 +2,10 @@ package com.groundzero.camw.core.errorhandlers
 
 import com.groundzero.camw.core.network.NetworkResponse
 import com.groundzero.camw.utils.END_POINT_NOT_FOUND
+import com.groundzero.camw.utils.INVALID_REQUEST
 import com.groundzero.camw.utils.code
 import org.springframework.http.HttpStatus
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -16,5 +18,11 @@ class GlobalControllerAdvice {
     @ExceptionHandler(NoHandlerFoundException::class)
     fun onNotFoundException(): NetworkResponse {
         return NetworkResponse.Error(code(HttpStatus.NOT_FOUND), END_POINT_NOT_FOUND)
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun onBadRequestException(): NetworkResponse {
+        return NetworkResponse.Error(code(HttpStatus.BAD_REQUEST), INVALID_REQUEST)
     }
 }
--- src/main/kotlin/com/groundzero/camw/utils/Constants.kt
+++ src/main/kotlin/com/groundzero/camw/utils/Constants.kt
@@ -3,3 +3,4 @@ package com.groundzero.camw.utils
 const val INVALID_UID = "Invalid uid"
 const val INVALID_SECRET = "Invalid secret"
 const val END_POINT_NOT_FOUND = "Endpoint not found"
+const val INVALID_REQUEST = "Invalid request"
